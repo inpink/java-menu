3c150ddd7328a9d0ad9fc9e49b031e290e391975
inpink <inpink@kakao.com>
2023-11-22 21:16:11 +0900

    feat: "양식" 카테고리의 메뉴

---
+++ src/main/java/menu/domain/menuItem/WesternCategory.java
@@ -0,0 +1,25 @@
+package menu.domain.menuItem;
+
+public enum WesternCategory implements MenuItem {
+
+    LASAGNA("라자냐"),
+    GRATIN("그라탱"),
+    GNOCCHI("뇨끼"),
+    QUICHE("끼슈"),
+    FRENCH_TOAST("프렌치 토스트"),
+    BAGUETTE("바게트"),
+    SPAGHETTI("스파게티"),
+    PIZZA("피자"),
+    PANINI("파니니");
+
+    private final String name;
+
+    WesternCategory(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
