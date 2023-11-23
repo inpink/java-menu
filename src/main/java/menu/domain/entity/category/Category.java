8adf750f0657c772b52b135328ba99138610781c
inpink <inpink@kakao.com>
2023-11-23 23:26:56 +0900

    feat: 컴퓨터는 "추천할 카테고리"를 무작위로 정한다.

--- src/main/java/menu/domain/entity/category/Category.java
+++ src/main/java/menu/domain/entity/category/Category.java
@@ -1,24 +1,29 @@
 package menu.domain.entity.category;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import menu.domain.util.ExceptionUtil;
+import menu.domain.util.RandomUtil;
 
 public enum Category {
-    JAPANESE("일식", Japanese.values()),
-    KOREAN("한식", Korean.values()),
-    CHINESE("중식", Chinese.values()),
-    ASIAN("아시안", Asian.values()),
-    WESTERN("양식", Western.values());
+    JAPANESE("일식", 1, Japanese.values()),
+    KOREAN("한식", 2, Korean.values()),
+    CHINESE("중식", 3, Chinese.values()),
+    ASIAN("아시안", 4, Asian.values()),
+    WESTERN("양식", 5, Western.values());
 
     private final String categoryName;
+    private final int number;
     private final List<CategoryItem> dishes;
 
-    Category(String categoryName, CategoryItem[] dishes) {
+    Category(String categoryName, int number, CategoryItem[] dishes) {
         this.categoryName = categoryName;
+        this.number = number;
         this.dishes = Arrays.stream(dishes)
                 .collect(Collectors.toList());
     }
@@ -32,11 +37,42 @@ public enum Category {
                 .orElseThrow(() -> ExceptionUtil.returnInvalidValueException());
     }
 
+    public static Category pickRandomCategory() {
+        int randomNum = RandomUtil.pickNumberInRange(getMinNumber(), getMaxNumber());
+
+        return Arrays.stream(Category.values())
+                .filter(category -> category.number == randomNum)
+                .findFirst()
+                .orElseThrow(() -> ExceptionUtil.returnInvalidValueException());
+    }
+
+    private static IntStream getCategoryNumbersStream() {
+        return Arrays.stream(Category.values())
+                .mapToInt(category -> category.number);
+    }
+
+    private static int getMinNumber() {
+        return getCategoryNumbersStream()
+                .min()
+                .orElseThrow(() -> ExceptionUtil.returnInvalidValueException());
+    }
+
+    private static int getMaxNumber() {
+        return getCategoryNumbersStream()
+                .max()
+                .orElseThrow(() -> ExceptionUtil.returnInvalidValueException());
+    }
+
     private static Optional<CategoryItem> findDishStream(String itemName) {
         return Arrays.stream(Category.values())
                 .flatMap(category -> category.getDishes().stream())
                 .filter(dish -> dish.getName().equalsIgnoreCase(itemName))
                 .findFirst();
+    }
+
+    public CategoryItem pickRandomItem() {
+        String menu = Randoms.shuffle(menus).get(0);
+
     }
 
     public String getCategoryName() {
---
+++ src/main/java/menu/domain/util/RandomUtil.java
@@ -0,0 +1,12 @@
+package menu.domain.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomUtil {
+
+    public static int pickNumberInRange(int min, int max) {
+        return Randoms.pickNumberInRange(min, max);
+    }
+
+
+}
