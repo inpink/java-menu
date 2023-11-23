79b60bbfff6909b03e0113f40e19846d1b33f0c6
inpink <inpink@kakao.com>
2023-11-24 01:36:36 +0900

    feat: 모든 추천 내용 출력

--- src/main/java/menu/domain/messages/OutputMessages.java
+++ src/main/java/menu/domain/messages/OutputMessages.java
@@ -3,7 +3,10 @@ package menu.domain.messages;
 public enum OutputMessages {
     RECOMMEND_RESULT_START("메뉴 추천 결과입니다."),
     RECOMMEND_RESULT_SORT("구분"),
-    RECOMMEND_RESULT_CATEGORY("카테고리");
+    RECOMMEND_RESULT_CATEGORY("카테고리"),
+    RECOMMEND_RESULT_END("추천을 완료했습니다.");
+
+
 
     private final String message;
 
--- src/main/java/menu/domain/view/OutputView.java
+++ src/main/java/menu/domain/view/OutputView.java
@@ -1,12 +1,14 @@
 package menu.domain.view;
 
 import static menu.domain.messages.OutputMessages.RECOMMEND_RESULT_CATEGORY;
+import static menu.domain.messages.OutputMessages.RECOMMEND_RESULT_END;
 import static menu.domain.messages.OutputMessages.RECOMMEND_RESULT_SORT;
 import static menu.domain.messages.OutputMessages.RECOMMEND_RESULT_START;
 
 import java.util.List;
 import java.util.Map;
 import menu.domain.dto.RecommendDto;
+import menu.domain.util.OutputUtil;
 
 public class OutputView {
 
@@ -19,6 +21,7 @@ public class OutputView {
         outputSortInformation(sorts);
         outputCategoryInformation(categories);
         outputCoachesItemsInformation(coachesItems);
+        outputRecommendationEndMessage();
     }
 
     private void outputRecommendationStartMessage() {
@@ -26,16 +29,31 @@ public class OutputView {
     }
 
     private void outputSortInformation(List<String> sorts) {
-        System.out.println(RECOMMEND_RESULT_SORT.getMessage());
+        System.out.println(formatWithBrackets(
+                RECOMMEND_RESULT_SORT.getMessage(),
+                sorts));
     }
 
     private void outputCategoryInformation(List<String> categories) {
-        System.out.println(RECOMMEND_RESULT_CATEGORY.getMessage());
+        System.out.println(formatWithBrackets(
+                RECOMMEND_RESULT_CATEGORY.getMessage(),
+                categories));
     }
 
     private void outputCoachesItemsInformation(Map<String, List<String>> coachesItems) {
-
+        coachesItems.forEach((coachName, items) -> {
+            System.out.println(formatWithBrackets(coachName, items));
+        });
     }
 
+    private void outputRecommendationEndMessage() {
+        OutputUtil.printEmptyLine();
+        System.out.println(RECOMMEND_RESULT_END.getMessage());
+    }
+
+    public String formatWithBrackets(String label, List<String> items) {
+        String formattedItems = String.join(" | ", items);
+        return "[ " + label + " | " + formattedItems + " ]";
+    }
 
 }
