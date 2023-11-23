f47452ac49f20253c33dbd840b7d3af59b568e07
inpink <inpink@kakao.com>
2023-11-23 22:16:34 +0900

    refactor: 분리자 ,를 상수화

--- src/main/java/menu/domain/messages/InputMessages.java
+++ src/main/java/menu/domain/messages/InputMessages.java
@@ -1,8 +1,10 @@
 package menu.domain.messages;
 
 public enum InputMessages {
+    SEPARATOR(","),
     START("점심 메뉴 추천을 시작합니다."),
-    INPUT_COACHES_NAMES("코치의 이름을 입력해 주세요. (, 로 구분)"),
+    INPUT_COACHES_NAMES(String.format(
+            "코치의 이름을 입력해 주세요. (%s 로 구분)",SEPARATOR)),
     INPUT_RESTRICTIONS("(이)가 못 먹는 메뉴를 입력해 주세요.");
 
     private final String message;
