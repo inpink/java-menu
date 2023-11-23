c5717b822d84126f607881ec61620c7b77e6cbfb
inpink <inpink@kakao.com>
2023-11-23 21:55:48 +0900

    feat: 양식 카테고리

---
+++ src/main/java/menu/domain/entity/category/Western.java
@@ -0,0 +1,23 @@
+package menu.domain.entity.category;
+
+public enum Western  implements CategoryItem{
+    LASAGNA("라자냐"),
+    GRATIN("그라탱"),
+    GNOCCHI("뇨끼"),
+    QUICHE("끼슈"),
+    FRENCH_TOAST("프렌치 토스트"),
+    BAGUETTE("바게트"),
+    SPAGHETTI("스파게티"),
+    PIZZA("피자"),
+    PANINI("파니니");
+
+    private final String koreanName;
+
+    Western(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    public String getKoreanName() {
+        return koreanName;
+    }
+}
